0038f67339cc413a08758497103f2916b87f391a
DinoStealyobih <fahreizaas@gmail.com>
2024-11-15 08:20:23 +0700

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Wait 3 seconds before redirecting to main page
+    setTimeout(() => {
+        window.location.href = "index.html"; // Redirect to main page
+    }, 3000); // Adjust timing if needed to match the loading bar
+});
